315acaf5d1196b1f8a931db1768900cb639a3517
tzahirahmed <tzahirahmed@flexera.com>
2023-03-20 19:19:10 +0530

    solution1 & solution2

---
+++ src/main/java/org/techzoo/leetcode/Solution01.java
@@ -0,0 +1,28 @@
+package org.techzoo.leetcode;
+
+import java.util.*;
+public class Solution01 {
+
+    public static void main(String[] args) {
+
+        int[] result = new Solution01().twoSum(new int[]{1,2,3,5,6,7}, 10);
+        System.out.println(result[0] + " , " + result[1]);
+
+    }
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                break;
+            }
+            map.put(nums[i],i);
+        }
+
+        return result;
+    }
+
+}
---
+++ src/main/java/org/techzoo/leetcode/Solution02.java
@@ -0,0 +1,38 @@
+package org.techzoo.leetcode;
+
+public class Solution02 {
+    //https://leetcode.com/problems/can-place-flowers/
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if(flowerbed[i] == 0 && canPlant(flowerbed, i)) {
+                flowerbed[i] = 1;
+                count++;
+                if(count == n) {
+                    return true;
+                }
+
+            }
+        }
+
+        return false;
+    }
+
+    private boolean canPlant(int[] flowerbed, int i) {
+        if((i-1>=0 && flowerbed[i-1] == 1) || (i+1<flowerbed.length && flowerbed[i+1] ==1)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] flowerbed = new int[]{1,0,0,0,1};
+        int n = 1;
+        System.out.println("Can Plant Flowers ? "+new Solution02().canPlaceFlowers(flowerbed, n));
+    }
+}
